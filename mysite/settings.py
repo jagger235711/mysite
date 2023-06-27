41b768979f5c205598a692824374c58549373500
巫文杰 <252587809u@qq.com>
2023-06-27 17:34:11 +0800

    写完测试用例

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC+8'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
